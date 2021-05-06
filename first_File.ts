1ce9e80ed7ddc67a5ed878593f799586602ff97e
Ninja Coder <49287322+rathodsuraj@users.noreply.github.com>
2021-05-06 17:48:23 +0530

    Ts file is updated

---
+++ first_File.ts
@@ -0,0 +1,24 @@
+// var name:string='suraj';
+
+var msg:string="hello suraj you are there";
+console.log(msg);
+
+
+var no1:number=10;
+var no2:number=20;
+var add:number=no1+no2;
+console.log(add);
+
+var isActive:boolean
+console.log(isActive);
+
+var anyValue:any;
+anyValue='String Value';
+console.log(anyValue);
+
+anyValue=100;
+console.log(anyValue);
+
+const variable=23;
+console.log(variable);
+
